66aa6043c4bc4c6153a00c44d1565429fb8c00a9
sumedh <summm02@gmail.com>
2022-03-07 15:12:04 -0500

    Added support for initial queries

--- backend/MoViz/movizApp/urls.py
+++ backend/MoViz/movizApp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('graph/', views.request_resolver, name='request_resolver'),
     path('', views.index, name='index'),
 ]

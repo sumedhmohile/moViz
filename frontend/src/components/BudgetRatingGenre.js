665b7186a6d6e9d8a2959e4dcb128dc6790c0345
Vipul Gharde <vipul.gharde@gmail.com>
2022-04-07 05:08:53 -0400

    Added scatterplot legend

--- frontend/src/components/BudgetRatingGenre.js
+++ frontend/src/components/BudgetRatingGenre.js
@@ -2,96 +2,38 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Plot from "react-plotly.js";
 
-function getRandomColor() {
-  var letters = "0123456789ABCDEF";
-  var color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-function fetchGraphData(xSetter, ySetter, genreSetter, nameSetter) {
-  axios
-    .post("/moviz/graph/", { graphID: "budgetRatingGenre" })
-    .then((response) => {
-      console.log(response.data);
-
-      var dataArray = response.data.data;
-
-      let xArray = [];
-      let yArray = [];
-      let genreArray = [];
-      let nameArray = [];
-      let colourDict = {};
-
-      for (let i = 0; i < dataArray.length; ++i) {
-        colourDict[dataArray[i].name] = getRandomColor();
-      }
-
-      for (let i = 0; i < dataArray.length; ++i) {
-        //                    resultDict[dataArray[i].genre][xData].push()
-
-        xArray.push(dataArray[i].budget);
-        yArray.push(dataArray[i].vote_average);
-        genreArray.push(colourDict[dataArray[i].name]);
-        nameArray.push(dataArray[i].title);
-      }
-
-      xSetter(xArray);
-      ySetter(yArray);
-      genreSetter(genreArray);
-      nameSetter(nameArray);
-
-      console.log("CHECK");
-      console.log(colourDict["Adventure"]);
-    });
-}
-
 export const BudgetRatingGenre = () => {
-  const [xDataGetter, xDataSetter] = useState(0);
-  const [yDataGetter, yDataSetter] = useState(0);
-  const [genreGetter, genreDataSetter] = useState(0);
-  const [nameGetter, nameDataSetter] = useState(0);
+  const [graphData, setGraphData] = useState([]);
 
   useEffect(() => {
-    console.log("xData changed!");
-  }, [xDataGetter]);
+    axios
+      .post("/moviz/graph/", { graphID: "budgetRatingGenre" })
+      .then((response) => {
+        let genres = new Set(response.data.data.map((x) => x.name));
+        let data = [];
 
-  useEffect(() => {
-    console.log("yData changed!");
-  }, [yDataGetter]);
+        for (let genre of Array.from(genres).sort()) {
+          let genre_data = response.data.data.filter((x) => x.name === genre);
+          data.push({
+            x: genre_data.map((x) => x.budget),
+            y: genre_data.map((x) => x.vote_average),
+            mode: "markers",
+            type: "scatter",
+            name: genre,
+            text: genre_data.map((x) => x.title),
+          });
+        }
 
-  useEffect(() => {
-    console.log("genreData changed!");
-    console.log(genreGetter);
-  }, [genreGetter]);
-
-  useEffect(() => {
-    console.log("nameData changed!");
-  }, [nameGetter]);
-
-  useEffect(() => {
-    console.log("I have been mounted");
-    fetchGraphData(xDataSetter, yDataSetter, genreDataSetter, nameDataSetter);
+        setGraphData(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
-
-  var plotData = [
-    {
-      type: "scatter",
-      x: xDataGetter,
-      y: yDataGetter,
-      mode: "markers",
-      text: nameGetter,
-      marker: {
-        color: genreGetter,
-      },
-    },
-  ];
 
   return (
     <Plot
-      data={plotData}
+      data={graphData}
       layout={{
         title: "Movie Budget vs. Rating vs. Genre",
         xaxis: { title: "Budget" },

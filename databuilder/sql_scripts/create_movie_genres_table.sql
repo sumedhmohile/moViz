021e0648d7a709115d08f3c5e4d5af38ecc99faf
Vipul Gharde <vipul.gharde@gmail.com>
2022-04-27 00:41:52 -0400

    Change order of composite primary keys

--- databuilder/sql_scripts/create_movie_genres_table.sql
+++ databuilder/sql_scripts/create_movie_genres_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS movie_genres
 (
-  genre_id INT NOT NULL,
   movie_id INT NOT NULL,
-  PRIMARY KEY (genre_id, movie_id),
-  FOREIGN KEY (genre_id) REFERENCES genres(genre_id) ON DELETE CASCADE,
-  FOREIGN KEY (movie_id) REFERENCES movies(movie_id) ON DELETE CASCADE
+  genre_id INT NOT NULL,
+  PRIMARY KEY (movie_id, genre_id),
+  FOREIGN KEY (movie_id) REFERENCES movies(movie_id) ON DELETE CASCADE,
+  FOREIGN KEY (genre_id) REFERENCES genres(genre_id) ON DELETE CASCADE
 );
 
--- databuilder/sql_scripts/create_movie_production_companies_table.sql
+++ databuilder/sql_scripts/create_movie_production_companies_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS movie_production_companies
 (
-  production_company_id INT NOT NULL,
   movie_id INT NOT NULL,
-  PRIMARY KEY (production_company_id, movie_id),
-  FOREIGN KEY (production_company_id) REFERENCES production_companies(production_company_id) ON DELETE CASCADE,
-  FOREIGN KEY (movie_id) REFERENCES movies(movie_id) ON DELETE CASCADE
+  production_company_id INT NOT NULL,
+  PRIMARY KEY (movie_id, production_company_id),
+  FOREIGN KEY (movie_id) REFERENCES movies(movie_id) ON DELETE CASCADE,
+  FOREIGN KEY (production_company_id) REFERENCES production_companies(production_company_id) ON DELETE CASCADE
 );
 

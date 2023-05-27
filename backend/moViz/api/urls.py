562653bd6086b6e5c1c425ff2fb831e42ed824df
Vipul Gharde <vipul.gharde@gmail.com>
2023-05-27 06:51:18 -0400

    Move files

--- backend/moViz/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('graph/', views.request_resolver, name='request_resolver'),
-    path('', views.index, name='index'),
-]
--- databuilder/sql_scripts/create_movie_languages_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS movie_languages
-(
-  movie_id INT NOT NULL,
-  iso_639_1 VARCHAR(255) NOT NULL,
-  PRIMARY KEY (movie_id, iso_639_1),
-  FOREIGN KEY (movie_id) REFERENCES movies(movie_id) ON DELETE CASCADE,
-  FOREIGN KEY (iso_639_1) REFERENCES languages(iso_639_1) ON DELETE CASCADE
-);
-
